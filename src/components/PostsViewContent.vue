f2e426b8741d3de8e3ab53ab1c5b59b1e1a8fea6
v43rus <leonard.jm@live.com>
2025-07-17 09:34:33 +0200

    Added basic Posts component

---
+++ src/components/PostsViewContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="w-full max-w-5xl mx-auto p-6! bg-black/70 border border-green-500 rounded-md shadow-lg text-white text-center space-y-4"
+  >
+    <h1 class="text-2xl md:text-3xl font-bold text-green-400">Welcome to The Posts</h1>
+
+    <p class="text-sm md:text-base">
+      Trending posts from <strong>HackerNews</strong> based on trending tags and user interactions.
+    </p>
+  </div>
+</template>
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MainNav from '@/components/MainNav.vue'
+import PostsViewContent from '@/components/PostsViewContent.vue'
 </script>
 
 <template>
   <main>
-    <h1 class="hidden">HackerNews Posts</h1>
     <img src="/logo.png" alt="HackerNews logo" class="logo w-[80px] sm:w-[120px] md:w-[160px]" />
     <MainNav />
+
+    <PostsViewContent />
   </main>
 </template>
